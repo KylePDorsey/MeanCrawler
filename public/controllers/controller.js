14f2c41ca42a8140a61a464d194e0e849bc2f224
KylePDorsey <kylepdorsey@gmail.com>
2017-06-13 19:46:28 -0500

    once data is submitted and saved to mongodb clear input form and update tabel

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,17 +1,18 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
 
     $http.get('/joblist').success(function(response){
-    	console.log("Got the data I requested.");
     	$scope.jobList = response;
     });
 
+
     $scope.addJob = function() {
-    	console.log($scope.job);
     	$http.post('/joblist', $scope.job).success(function(response){
-    		console.log(response);
+    		$scope.job = "";
+    		$http.get('/joblist').success(function(response){
+		    	$scope.jobList = response;
+		    });
     	});
-    }
+    };
 
 }]);
