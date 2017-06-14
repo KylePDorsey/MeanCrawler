f370f9c58cf87a4246f3201829209c2f65690a4e
KylePDorsey <kylepdorsey@gmail.com>
2017-06-13 19:14:23 -0500

    Push data from mongo to front end

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('joblist', ['joblist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/joblist', function(req, res){
 	console.log("Got the GET request")
 
-	job1 = {
-    	companyName: 'HP',
-    	companyLocation: 'Overland Park KS',
-    	jobTitle: 'IT Guy',
-    	jobSummary: 'Just fix the keyboards when they break'
-    };
-
-    job2 = {
-    	companyName: 'Torch',
-    	companyLocation: 'Leawood KS',
-    	jobTitle: 'Crawler Craziness',
-    	jobSummary: 'Crawl the world and learn some things'
-    };
-
-    job3 = {
-    	companyName: 'McDonalds',
-    	companyLocation: 'Olathe KS',
-    	jobTitle: 'Fry Cook',
-    	jobSummary: 'Cook some fries and eat some fries'
-    };
-
-    var jobList = [job1, job2, job3];
-
-    res.json(jobList);
+	db.joblist.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	})
 });
 
 app.listen(8080);
