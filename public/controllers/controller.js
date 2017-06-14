8149fdda9a4e0fd86c4ca99a351d1b7d549730c8
KylePDorsey <kylepdorsey@gmail.com>
2017-06-13 19:01:50 -0500

    Establish data from from server to front end

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,10 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+
+    $http.get('/joblist').success(function(response){
+    	console.log("Got the data I requested.");
+    	$scope.jobList = response;
+    });
+
 }]);
